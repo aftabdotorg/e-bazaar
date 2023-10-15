1aace0f026ee7897cf1f05b664e2829d925a4411
Aftab Akhand <aft4b.akhand@gmail.com>
2023-10-15 17:10:11 +0530

    ready

--- app/src/components/Product/productAPI.js
+++ app/src/components/Product/productAPI.js
@@ -66,32 +66,3 @@ export function fetchAllBrands() {
   });
 }
 
-// create product admin
-
-export function addProduct(productOBJ) {
-  return new Promise(async (resolve) => {
-    const response = await fetch(`http://localhost:8080/products/`, {
-      method: "POST",
-      body: JSON.stringify(productOBJ),
-      headers: { "content-type": "application/json" },
-    });
-    const data = await response.json();
-    resolve({ data });
-  });
-}
-
-// delete product
-
-export function deleteProduct(productID) {
-  return new Promise(async (resolve) => {
-    const response = await fetch(
-      "http://localhost:8080/products/" + productID,
-      {
-        method: "DELETE",
-        headers: { "content-type": "application/json" },
-      }
-    );
-    const data = await response.json();
-    resolve({ data: { id: productID } });
-  });
-}
